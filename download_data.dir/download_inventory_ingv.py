9d1122b27d0a1414cd4beb2028b65921c6a97737
avuan <avuan@inogs.it>
2017-11-06 11:00:35 +0100

    reformat code following pep8

--- download_data.dir/download_inventory_ingv.py
+++ download_data.dir/download_inventory_ingv.py
@@ -12,5 +12,5 @@ endtime = UTCDateTime("2017-06-30")
 
 inventory = client.get_stations(
     starttime=starttime, endtime=endtime,
-    network="IV,MN", sta="*", channel="*", filename="inv.ingv", format='xml') 
+    network="IV,MN", sta="*", channel="*", filename="inv.ingv", format='xml')
 print(inventory)
