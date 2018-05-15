0965bea45f45cc08bce8e6fec7ec3470ef542a9d
vuan <avuan@inogs.it>
2018-05-15 12:16:36 +0200

    changed lista1 read cutting the blank line at the end of file

--- trim_templates.dir/trim_templates4.1.py
+++ trim_templates.dir/trim_templates4.1.py
@@ -96,7 +96,7 @@ fname = "%s" % day_list
 # array of days is built deleting last line character (/newline) ls -1 command
 # include a newline character at the end
 with open(fname) as fl:
-    days = [line[:-1] for line in fl]
+    days = [line for line in fl]
     print(days)
 
 fl.close()
