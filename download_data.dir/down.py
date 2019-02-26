f418520907b8eb880bba5e9d3fe29a569ce753d6
vuan <avuan@inogs.it>
2019-02-26 13:13:53 +0100

    put some files

---
+++ download_data.dir/down.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+
+import time
+import os
+from obspy.clients.fdsn import Client
+from obspy.core.utcdatetime import UTCDateTime
+
+client = Client("INGV")
+networks = ["IV"]
+#
+stations=["SSFR"]
+
+channels = ["EH*", "HH*"]
+
+start = "2012-06-25T00:00:00.000"
+stop = "2012-06-27T00:00:00.000"
+
+# 24h as seconds
+chuncklength = 86400
+
+# output directory
+inp_dir = "./24h/"
+
+# output directory
+if not os.path.exists(inp_dir):
+    os.makedirs(inp_dir)
+# ---------------------------do not change below
+
+for sta in stations:
+    t1 = (UTCDateTime(start)).timestamp
+    t3 = (UTCDateTime(stop)).timestamp
+    t2 = t1 + chuncklength
+
+    while t2 <= t3:
+        print("station == ", sta)
+
+        for net in networks:
+
+            for chann in channels:
+
+                try:
+                    bulk = [(net, sta, "*", chann, UTCDateTime(
+                        t1), UTCDateTime(t2))]
+                    print("bulk == ", bulk)
+                    yy = str(UTCDateTime(t1).year)
+                    mm = str(UTCDateTime(t1).month).zfill(2)
+                    dd = str(UTCDateTime(t1).day).zfill(2)
+
+                    newfile = inp_dir + yy + mm + dd +\
+                        "." + sta + "." + chann[0:3]
+                    client.get_waveforms_bulk(bulk, filename=newfile)
+                    time.sleep(2)
+                except Exception:
+                    time.sleep(1)
+                    pass
+
+        t1 = t1 + chuncklength
+        t2 = t2 + chuncklength
---
+++ template_check.dir/test_new.dir/test_kurtosis2.py
@@ -0,0 +1,97 @@
+from obspy import *
+from scipy.stats import kurtosis
+from subprocess import call
+import matplotlib.pyplot as plt 
+import os.path
+import numpy as np
+from obspy.signal.filter import envelope
+
+# FlagPrint==0 no printout, FlagPrint==1 check waveforms 
+FlagPrint=0
+
+st = Stream()
+tr = Trace()
+call(['rm', '-r', './bad'])
+call(['rm', '-r', './good'])
+call(['mkdir', './bad'])
+call(['mkdir', './good'])
+channels = ['EHE', 'EHN', 'EHZ', 'HHE','HHN', 'HHZ']
+#stations = ['ATPI', 'ATCC', 'ATLO', 'ATPC', 'ATVO', 'FOSV', 'FRON', 'NARO', 'PARC', 'SSFR']
+stations = ['NRCA']
+networks = ['IV', 'MN']
+
+for s in stations:
+
+    for c in channels: 
+
+        for n in networks:
+
+            for i in range(0, 570):
+                inpfile = str(i) + '.' + n + '.' + s + '..' + c + '.mseed'
+                try:
+                    if os.path.isfile(inpfile):
+                        st = read(inpfile)
+                        tr = st[0]
+                        dt = tr.stats.delta
+                        npts = tr.stats.npts
+                        #print(npts)
+                        t = np.linspace(0, (npts-1)*dt, npts)
+                        #print(t)
+                        amaxa = max(tr.data)
+
+                        # first test
+                        # check if we are in the seismogram coda 
+                        # maximum absolute amplitude is at the beginning
+                        index_max = np.argmax(abs(tr.data))
+                        pp = int(npts * 0.3) 
+
+                        if index_max<=pp:
+                            indm = 0
+                        elif index_max>pp:
+                            indm = 1
+
+                        # second test
+                        # check if maximum amplitude is in the last 5% of coda points
+                        pp95 = int(npts * 0.95)
+                        if index_max>=pp95:
+                            indm95 = 0
+                        elif index_max<pp95:
+                            indm95 = 1
+                        
+                        # third test
+                        spike = 0
+                        astd = 5 * np.std(tr.data)
+
+                        if amaxa > astd:
+                            print('spike detetcted')
+                            spike = 1
+                        
+                        #tre = envelope(tr.data)
+                        k = kurtosis(abs(tr.data), axis=0, fisher=True, bias=True, nan_policy='propagate')
+                        kstring = 'kurt(Pearson) = ' + str(k)[0:6] + ' ind1 = ' + str(indm) + ' ind2 = ' + str(indm95)
+                        #print(inpfile, k, indm, indm95)
+
+                        if k<=0.8 or indm==0 or indm95==0 or spike==1: 
+                            call(['cp', inpfile, './bad'])
+                            print(inpfile, k, indm, indm95)
+                        elif k>0.8 and indm==1 and indm95==1 and spike==0:
+                            call(['cp', inpfile, './good'])
+
+                        if FlagPrint==1:
+                            fig = plt.figure(figsize=(8,3))
+                            ax = fig.add_subplot(111)
+                            ax.set_title(inpfile)
+
+                            if k<=0.8 or indm==0 or indm95==0 or spike==1:
+                                #ax.plot(t, abs(tr.data), 'r')
+                                ax.plot(t, tr.data, 'r')
+                            elif k>0.8 and indm==1 and indm95==1 and spike==0:
+                                #ax.plot(t, abs(tr.data), 'k')
+                                ax.plot(t, tr.data, 'k')
+                            ax.set_xlabel('Time[s]')
+                            ax.text(.5, 0.9*amaxa, kstring, fontsize=15)
+                            plt.show()
+                except:
+                    print("warning file not found ", inpfile)
+                    pass
+                     
