f0354d4d608eec2003bab3965b17413eb739246c
vuan <avuan@inogs.it>
2019-10-04 12:32:13 +0200

    modified postproc37.sh to create by itself a list of days to process

--- output.process_detections.dir/postproc37.sh
+++ output.process_detections.dir/postproc37.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+
+# set start abd stop dates
+start="2009-03-29"
+stop="2009-03-30"
+
+# generate a list of days to process
+until [[ $start > $stop ]]; do
+    echo $start 
+    list+=`echo "${start:2:2}${start:5:2}${start:8:2} "`
+    start=$(gdate -I -d "$start+ 1 day")
+done
+
 #collect catalogs obtained using different templates to provide a daily catalog
-for day in 090330
+for day in $list
 do
 find ./ -maxdepth 1 -name "*.${day}.cat" -print0 | xargs -0 cat > ${day}Acat
 	
