1e00088ab0fa2ac1b9932b1803386142531f3394
Aessandro Vuan <avuan@inogsit>
2021-02-18 11:22:19 +0100

    main changes in create_templates.py to reduce time

--- input.create_templates.dir/create_templates.py
+++ input.create_templates.dir/create_templates.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
+
 import numpy as np
 import glob
-import pandas as pd
 import datetime
 from obspy import read
+from time import perf_counter
 from obspy import read_inventory, read_events, Stream, Trace
 from obspy.core.utcdatetime import UTCDateTime
 from obspy.geodetics import gps2dist_azimuth
@@ -13,49 +13,6 @@ from obspy.taup.taup_create import build_taup_model
 from obspy.taup.tau import TauPyModel
 from obspy.io.zmap.core import _is_zmap
 from obspy.io.quakeml.core import _is_quakeml
-
-
-def listdays(year, month, day, period):
-    # create a list of days for scanning by templates
-    datelist = pd.date_range(
-        datetime.datetime(year, month, day), periods=period
-    ).tolist()
-    a = list(map(pd.Timestamp.to_pydatetime, datelist))
-    days_from_par = []
-    for i in a:
-        days_from_par.append(i.strftime("%y%m%d"))
-    return days_from_par
-
-
-def create_day_list(catalog, days_from_par):
-    # creates day list from the intersection between the datepriod input in trim.par and
-    # the template catalog
-    days = []
-    days_from_catalog = []
-    num_eve = len(catalog)
-    print(catalog)
-    print(num_eve)
-    dd = [catalog[i].origins[0].time.date for i in range(0, num_eve)]
-    dd = list(set(dd))
-
-    for day in dd:
-        year = str(day.strftime("%y"))
-        month = str(day.strftime("%m"))
-        dday = str(day.strftime("%d"))
-        days_from_catalog += [year + month + dday]
-
-    print(days_from_catalog, days_from_par)
-
-    days = list(
-        sorted(
-            set(days_from_par).intersection(days_from_catalog),
-            key=lambda x: days_from_par.index(x),
-        )
-    )
-
-    print(days)
-    return days
-
 
 def kilometer2degrees(kilometer, radius=6371):
     """
@@ -87,9 +44,9 @@ def read_input_par(trimfile):
     tlen_bef = float(data[21])
     tlen_aft = float(data[22])
     utc_prec = int(data[23])
-    cont_dir = "./" + data[24] + "/"
-    temp_dir = "./" + data[25] + "/"
-    dateperiod = data[26].split(" ")
+    time_length_window = int(data[24])
+    cont_dir = "./" + data[25] + "/"
+    temp_dir = "./" + data[26] + "/"
     ev_catalog = str(data[27])
     start_itemp = int(data[28])
     stop_itemp = int(data[29])
@@ -105,9 +62,9 @@ def read_input_par(trimfile):
         tlen_bef,
         tlen_aft,
         utc_prec,
+        time_length_window,
         cont_dir,
         temp_dir,
-        dateperiod,
         ev_catalog,
         start_itemp,
         stop_itemp,
@@ -131,11 +88,10 @@ def read_sta_inv(invfile, sta):
     return lat, lon, elev
 
 
+start_time = perf_counter()
 trimfile = "./trim.par"
 
 
-# lat, lon, elev = read_sta_inv(invfile, station)
-# print(lat, lon, elev)
 
 [
     stations,
@@ -146,9 +102,9 @@ trimfile = "./trim.par"
     tlen_bef,
     tlen_aft,
     utc_prec,
+    time_length_window,
     cont_dir,
     temp_dir,
-    dateperiod,
     ev_catalog,
     start_itemp,
     stop_itemp,
@@ -198,151 +154,135 @@ st2 = Stream()
 tvel = "./" + taup_model + ".tvel"
 build_taup_model(tvel)
 
-# generate list of days to process
-year = int(dateperiod[0])
-month = int(dateperiod[1])
-day = int(dateperiod[2])
-period = int(dateperiod[3])
-days_from_par = listdays(year, month, day, period)
-days = create_day_list(cat, days_from_par)
+for iev in range(start_itemp, stop_itemp):
+    ot = cat[iev].origins[0].time.datetime
+    ot1 = UTCDateTime(ot)
+    yy = ot1.year
+    mm = ot1.month
+    dd = ot1.day
+    hh = ot1.hour
+    minu = ot1.minute
+    sec = ot1.second
 
-for ista in stations:
 
-    for day in days:
-        print("day == ", day)
-        inpfiles = cont_dir + day + "." + ista + ".???"
-        st.clear()
+    if _is_zmap(ev_catalog):
 
-        for file in glob.glob(inpfiles):
-            st += read(file)
+        if ncat == 1:
+            microsec = aa3
 
-        st.merge(method=1, fill_value=0)
-        st.detrend("constant")
-        st.filter("bandpass", freqmin=bandpass[0], freqmax=bandpass[1], zerophase=True)
-        dataYY = int("20" + day[0:2])
-        dataMM = int(day[2:4])
-        dataDD = int(day[4:6])
+        else:
+            microsec = aa3[iev]
+
+    else:
+        microsec = ot1.microsecond
+
+    microsec = int(microsec)
+    m = cat[iev].magnitudes[0].mag
+    lon = cat[iev].origins[0].longitude
+    lat = cat[iev].origins[0].latitude
+    dep = cat[iev].origins[0].depth / 1000
+    eve_coord = [lat, lon, dep]
+    print(eve_coord)
+
+    ot0 = UTCDateTime(yy, mm, dd, hh, minu, sec, microsec)
+
+    # set startime and stoptime values to reduce memory charge
+    ot_start =  UTCDateTime(yy, mm, dd, hh, minu, sec, microsec) - time_length_window
+    ot_stop = UTCDateTime(yy, mm, dd, hh, minu, sec, microsec) + time_length_window
+
+    day = str(yy)[2:4] + str(mm).zfill(2) + str(dd).zfill(2)
+    print("day == ", day)
+    inpfiles = cont_dir + day + "." + "*.???"
+
+
+    st.clear()
+
+    for file in glob.glob(inpfiles):
+        st += read(file, starttime=ot_start, endtime=ot_stop)
+
+    st.merge(method=1, fill_value=0)
+    st.detrend("constant")
+    st.filter(
+              "bandpass",
+              freqmin=bandpass[0],
+              freqmax=bandpass[1],
+              zerophase=True
+              )
+    print(st)
+
+    for tr in st:
+
+        ista = tr.stats.station
+        network = tr.stats.network
+        channel = tr.stats.channel
 
         # to avoid errors in the input trim.par at stop_itemp
-        if stop_itemp > ncat:
-            stop_itemp = ncat
+        print("ista, network == ", ista, network)
 
-        for iev in range(start_itemp, stop_itemp):
-            ot = cat[iev].origins[0].time.datetime
-            ot1 = UTCDateTime(ot)
-            yy = ot1.year
-            mm = ot1.month
-            dd = ot1.day
-            hh = ot1.hour
-            minu = ot1.minute
-            sec = ot1.second
+        invfile = "./inv." + network
+        slat, slon, selev = read_sta_inv(invfile, ista)
+        print(ista, slat, slon, selev)
 
-            if _is_zmap(ev_catalog):
+        if slat != 999:
+            print("Station ", ista, " found in inventory ", invfile)
+            #break
+        else:
+            print("Warning no data found in ", invfile, " for station", ista)
+            continue
 
-                if ncat == 1:
-                    microsec = aa3
+        eve_lat = eve_coord[0]
+        eve_lon = eve_coord[1]
+        eve_dep = eve_coord[2]
 
-                else:
-                    microsec = aa3[iev]
+        if eve_dep < 1.5:
+            eve_dep = 1.5
 
-            else:
-                microsec = ot1.microsecond
+        epi_dist, az, baz = gps2dist_azimuth(eve_lat, eve_lon, slat, slon)
+        epi_dist = epi_dist / 1000
 
-            microsec = int(microsec)
-            m = cat[iev].magnitudes[0].mag
-            lon = cat[iev].origins[0].longitude
-            lat = cat[iev].origins[0].latitude
-            dep = cat[iev].origins[0].depth
-            # depth in km
-            dep = dep / 1000
-            ot0 = UTCDateTime(yy, mm, dd, hh, minu, sec, microsec)
-            ot2 = UTCDateTime(yy, mm, dd)
-            ot3 = UTCDateTime(dataYY, dataMM, dataDD)
+        if epi_dist < limit_epi_dist:
+            print("epi_dist==", epi_dist)
 
-            if ot2 == ot3:
-                eve_coord = [lat, lon, dep]
+            deg = kilometer2degrees(epi_dist)
+            print("deg==", deg)
+            print("eve_dep==", eve_dep)
+            model = TauPyModel(model=taup_model)
+            arrivals = model.get_travel_times(
+                source_depth_in_km=eve_dep,
+                distance_in_degree=deg,
+                phase_list=["s", "S"],
+                )
+            arrS = arrivals[0]
+            print("arrS.time=...", arrS.time)
 
-            print("ista", ista)
+            stime = UTCDateTime(ot0) + arrS.time - tlen_bef
+            print("stime", stime)
+            etime = UTCDateTime(ot0) + arrS.time + tlen_aft
+            print("etime", etime)
 
-            for network in networks:
-                invfile = "./inv." + network
-                slat, slon, selev = read_sta_inv(invfile, ista)
-                print(ista, slat, slon, selev)
-
-                if slat != 999:
-                    print("Station ", ista, " found in inventory ", invfile)
-                    break
-                else:
-                    print("Warning no data found in ", invfile, " for station", ista)
-                    continue
-
-            eve_lat = eve_coord[0]
-            eve_lon = eve_coord[1]
-            eve_dep = eve_coord[2]
-
-            if eve_dep < 1.5:
-                eve_dep = 1.5
-
-            epi_dist, az, baz = gps2dist_azimuth(eve_lat, eve_lon, slat, slon)
-            epi_dist = epi_dist / 1000
-
-            if epi_dist < limit_epi_dist:
-                print("epi_dist==", epi_dist)
-
-                deg = kilometer2degrees(epi_dist)
-                print("deg==", deg)
-                print("eve_dep==", eve_dep)
-                model = TauPyModel(model=taup_model)
-                arrivals = model.get_travel_times(
-                    source_depth_in_km=eve_dep,
-                    distance_in_degree=deg,
-                    phase_list=["s", "S"],
+            if tr.trim(stime, etime).__nonzero__():
+                print(tr)
+                netwk = tr.stats.network
+                ch = tr.stats.channel
+                tr.trim(stime, etime)
+                newfile = (
+                    temp_dir
+                    + str(iev)
+                    + "."
+                    + netwk
+                    + "."
+                    + ista
+                    + ".."
+                    + ch
+                    + ".mseed"
                     )
-                arrS = arrivals[0]
-                print("arrS.time=...", arrS.time)
-
-                stime = UTCDateTime(ot0) + arrS.time - tlen_bef
-                print("stime", stime)
-                etime = UTCDateTime(ot0) + arrS.time + tlen_aft
-                print("etime", etime)
-
-                # cut the 3-component template and save file
-                nchannels = len(channels)
-
-                for ichan in range(0, nchannels):
-                    print("ista", ista)
-                    st1.clear()
-                    # print("FILE", file)
-                    st1 = st.copy()
-                    tw = Trace()
-                    st2.clear()
-                    print(st1.select(station=ista, channel=channels[ichan]))
-                    st2 = st1.select(station=ista, channel=channels[ichan])
-
-                    if st2.__nonzero__():
-                        tw = st2[0]
-
-                        if tw.trim(stime, etime).__nonzero__():
-                            print(tw)
-                            netwk = tw.stats.network
-                            ch = tw.stats.channel
-                            tw.trim(stime, etime)
-                            newfile = (
-                                temp_dir
-                                + str(iev)
-                                + "."
-                                + netwk
-                                + "."
-                                + ista
-                                + ".."
-                                + ch
-                                + ".mseed"
-                            )
-                            print(newfile)
-                            tw.write(newfile, format="MSEED")
-                        else:
-                            pass
-                    else:
-                        pass
+                print(newfile)
+                print(tr.stats.mseed.encoding)
+                tr.write(newfile, format="MSEED")
             else:
                 pass
+        else:
+            pass
+    else:
+        pass
+print(" elapsed time ", perf_counter() - start_time, " seconds")
